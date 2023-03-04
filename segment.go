e04f1972f82059877d1413ef45a51bf76468d2ab
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-03-04 22:16:34 +0530

    function added to find first record

--- segment.go
+++ segment.go
@@ -1,9 +1,40 @@
 package main
 
+import "reflect"
+
 type Segment struct {
 	Root    *Segment `json:"-"`
 	Size    int
 	Path    string
 	Records []*Record
 	Sub     map[rune]*Segment
+}
+
+func (seg *Segment) FindFirstRecord(pattern map[string]interface{}) *Record {
+	var rV reflect.Value
+	var temp reflect.Value
+
+	hits := 0
+	desiredAmount := len(pattern)
+
+	for _, r := range seg.Records {
+		hits = 0
+
+		rV = reflect.ValueOf(r.Data)
+
+		for k, v := range pattern {
+			temp = rV.FieldByName(k)
+			if temp.IsValid() {
+				if reflect.DeepEqual(v, temp.Interface()) {
+					hits++
+				}
+			}
+		}
+
+		if hits >= desiredAmount {
+			return r
+		}
+	}
+
+	return nil
 }
