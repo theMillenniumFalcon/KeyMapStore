a7528fd1ad131f66381d71687cad246ccccc2b59
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-03-05 17:24:35 +0530

    added util functions

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "math/rand"
+
 var DB *Storage
 
 type Person struct {
@@ -7,4 +9,25 @@ type Person struct {
 	LastName  string
 	Age       int
 	City      string
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var lastRecord Person
+
+func randomString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+
+	return string(b)
+}
+
+func generateData(amount int) (persons []Person) {
+	persons = make([]Person, amount)
+	for i := 0; i < amount; i++ {
+		persons[i] = Person{randomString(4), randomString(3), rand.Int(), "Ottawa-" + randomString(2)}
+	}
+
+	return persons
 }
