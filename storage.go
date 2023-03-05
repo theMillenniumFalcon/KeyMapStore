502a4e8ae28260394bd1b1cfc92648c392955349
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-03-05 15:40:13 +0530

    added init functions

--- storage.go
+++ storage.go
@@ -4,3 +4,17 @@ type Storage struct {
 	Segments  map[rune]*Segment
 	IDCounter int64
 }
+
+func (db *Storage) Init() {
+	db.IDCounter = 0
+	db.Segments = make(map[rune]*Segment)
+}
+
+func (db *Storage) Size() (size int) {
+	size = 0
+	for _, s := range db.Segments {
+		size += s.Size
+	}
+
+	return size
+}
