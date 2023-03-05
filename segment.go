78cd670c3edc72c7b6c7eb00a6c64d8e9944397f
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-03-05 15:25:44 +0530

    added findRecords function

--- segment.go
+++ segment.go
@@ -38,3 +38,34 @@ func (seg *Segment) FindFirstRecord(pattern map[string]interface{}) *Record {
 
 	return nil
 }
+
+func (seg *Segment) FindRecords(pattern map[string]interface{}) []*Record {
+	var rV reflect.Value
+	var temp reflect.Value
+
+	hits := 0
+	desiredAmount := len(pattern)
+
+	records := make([]*Record, 0)
+
+	for _, r := range seg.Records {
+		hits = 0
+
+		rV = reflect.ValueOf(r.Data)
+
+		for k, v := range pattern {
+			temp = rV.FieldByName(k)
+			if temp.IsValid() {
+				if reflect.DeepEqual(v, temp.Interface()) {
+					hits++
+				}
+			}
+		}
+
+		if hits >= desiredAmount {
+			records = append(records, r)
+		}
+	}
+
+	return records
+}
