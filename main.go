1b2b12245047f1f11b44ec060d7e49ebb64a489a
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-03-07 19:07:01 +0530

    done!

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-var DB *Storage
+var db *Storage
 
 type Person struct {
 	FirstName string
@@ -19,7 +23,6 @@ func randomString(n int) string {
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
-
 	return string(b)
 }
 
@@ -28,6 +31,83 @@ func generateData(amount int) (persons []Person) {
 	for i := 0; i < amount; i++ {
 		persons[i] = Person{randomString(4), randomString(3), rand.Int(), "Ottawa-" + randomString(2)}
 	}
-
 	return persons
+}
+
+func writeData(records *[]Person) {
+	primaries := []string{"FirstName", "LastName"}
+	var err error
+
+	for _, x := range *records {
+		err = db.Write(GetIdentifier(x, primaries), x)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	lastRecord = (*records)[len(*records)-1]
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	// Initialization
+	db = &Storage{}
+	db.Init()
+
+	// Single write example
+	start := time.Now()
+	x := Person{"Daniil", "Furmanov", 10, "Moscow"}
+	db.Write(GetIdentifier(x, nil), x)
+	elapsed := time.Since(start)
+
+	fmt.Printf("Single write took %s\n", elapsed)
+
+	// Generate large amount of data
+	fmt.Println("Generating data...")
+	start = time.Now()
+	data := generateData(1000000)
+	elapsed = time.Since(start)
+
+	fmt.Printf("Generation took %s\n", elapsed)
+
+	// Write this data
+	fmt.Println("Writing the data...")
+	start = time.Now()
+	writeData(&data)
+	elapsed = time.Since(start)
+
+	fmt.Printf("Database now contains %d records. Writing took %s\n", db.Size(), elapsed)
+
+	// Search Example
+	searchPattern := map[string]interface{}{
+		"FirstName": lastRecord.FirstName,
+		"LastName":  lastRecord.LastName,
+	}
+
+	segment := GetIdentifierFromMap(searchPattern, []string{"FirstName", "LastName"})
+
+	start = time.Now()
+
+	fmt.Println("Searching data... segment " + segment)
+
+	seg, err := db.FindSegment(segment)
+	if err != nil {
+		fmt.Println("Segment not found")
+		return
+	}
+
+	fmt.Println("Segment found. Path:", seg.Path, "; Size:", seg.Size, "; Records count:", len(seg.Records))
+
+	records := seg.FindRecords(searchPattern)
+	elapsed = time.Since(start)
+
+	if len(records) > 0 {
+		fmt.Println(records[0].Data)
+	} else {
+		fmt.Println("Zero results found")
+	}
+
+	fmt.Printf("Search took %s\n", elapsed)
+
 }
--- storage.go
+++ storage.go
@@ -1,12 +1,18 @@
 package main
 
+import (
+	"errors"
+	"reflect"
+	"strconv"
+)
+
 type Storage struct {
 	Segments  map[rune]*Segment
-	IDCounter int64
+	IdCounter int64
 }
 
 func (db *Storage) Init() {
-	db.IDCounter = 0
+	db.IdCounter = 0
 	db.Segments = make(map[rune]*Segment)
 }
 
@@ -15,6 +21,172 @@ func (db *Storage) Size() (size int) {
 	for _, s := range db.Segments {
 		size += s.Size
 	}
-
 	return size
+}
+
+func (db *Storage) Write(id string, data interface{}) error {
+	if id == "" {
+		return errors.New("passed ID is invalid")
+	}
+
+	if data == nil {
+		return errors.New("passed data is nil")
+	}
+
+	var segID rune
+	var seg *Segment
+	var sub *Segment
+	ok := false
+
+	for i := 0; i < len(id); i++ {
+		segID = rune(id[i])
+		if seg == nil {
+			if seg, ok = db.Segments[segID]; !ok {
+				seg = &Segment{nil, 0, id[:i+1], make([]*Record, 0), make(map[rune]*Segment)}
+				db.Segments[segID] = seg
+			}
+		} else {
+			if sub, ok = seg.Sub[segID]; !ok {
+				sub = &Segment{seg, 0, id[:i+1], make([]*Record, 0), make(map[rune]*Segment)}
+				seg.Sub[segID] = sub
+				seg = sub
+			} else {
+				seg = sub
+			}
+		}
+
+	}
+
+	if seg == nil {
+		return errors.New("Segments were not evaluated properly for ID " + id)
+	}
+
+	db.IdCounter++
+	seg.Records = append(seg.Records, &Record{db.IdCounter, id, data})
+	// recursive incremental
+	seg.Size++
+	for seg.Root != nil {
+		seg.Root.Size++
+		seg = seg.Root
+	}
+
+	return nil
+}
+
+func (db *Storage) FindSegment(id string) (*Segment, error) {
+	if id == "" {
+		return nil, errors.New("passed ID is invalid")
+	}
+
+	var segmentID rune
+	var seg *Segment
+	ok := false
+
+	for i := 0; i < len(id); i++ {
+		segmentID = rune(id[i])
+		if seg == nil {
+			if seg, ok = db.Segments[segmentID]; !ok {
+				return nil, errors.New("segment not found")
+			}
+		} else {
+			if seg, ok = seg.Sub[segmentID]; !ok {
+				return nil, errors.New("subSegment not found")
+			}
+		}
+
+	}
+
+	return seg, nil
+}
+
+// primaryFields must be type of string or int
+func GetIdentifier(x interface{}, primaryFields []string) string {
+	v := reflect.Indirect(reflect.ValueOf(x))
+	t := v.Type()
+
+	l := v.NumField()
+
+	kinds := make([]reflect.Kind, l)
+	values := make([]interface{}, l)
+	keys := make([]reflect.StructField, l)
+
+	identifier := ""
+
+	for i := 0; i < l; i++ {
+		kinds[i] = v.Field(i).Kind()
+		values[i] = v.Field(i).Interface()
+		keys[i] = t.Field(i)
+	}
+
+	temp := ""
+
+	if primaryFields != nil {
+		for _, field := range primaryFields {
+			for i := 0; i < l; i++ {
+				if field == keys[i].Name {
+					if kinds[i] == reflect.String {
+						temp = values[i].(string)
+						if len(temp) > 0 {
+							identifier += string(temp[0])
+							break
+						}
+					} else if kinds[i] == reflect.Int {
+						identifier += string(strconv.Itoa(values[i].(int))[0])
+					}
+				}
+			}
+		}
+
+	} else {
+		for i := 0; i < l; i++ {
+			if kinds[i] == reflect.String {
+				temp = values[i].(string)
+				if len(temp) > 0 {
+					identifier = string(temp[0])
+					break
+				}
+			} else if kinds[i] == reflect.Int {
+				identifier += string(strconv.Itoa(values[i].(int))[0])
+			}
+		}
+	}
+
+	return identifier
+}
+
+// primaryFields must be type of string or int
+func GetIdentifierFromMap(m map[string]interface{}, primaryFields []string) string {
+	identifier := ""
+	temp := ""
+	var k reflect.Kind
+
+	if primaryFields != nil {
+		for _, field := range primaryFields {
+			v, ok := m[field]
+			if ok {
+				k = reflect.TypeOf(v).Kind()
+				if k == reflect.String {
+					temp = v.(string)
+					if len(temp) > 0 {
+						identifier += string(temp[0])
+					}
+				} else if k == reflect.Int {
+					identifier += string(strconv.Itoa(v.(int))[0])
+				}
+			}
+		}
+
+	} else {
+		for _, value := range m {
+			if reflect.TypeOf(value).Kind() == reflect.String {
+				temp = value.(string)
+				if len(temp) > 0 {
+					identifier = string(temp[0])
+					break
+				}
+			}
+		}
+	}
+
+	return identifier
 }
